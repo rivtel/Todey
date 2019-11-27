db7fad0c24edb911c220233e3eb54400f0e0ec99
river <shalomteles@gmail.com>
2019-11-27 11:36:01 -0500

    Convert to MVC Design Pattern

--- Todey/Controller/ViewController.swift
+++ Todey/Controller/ViewController.swift
@@ -12,13 +12,25 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
-        }
+        
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Fly a Kite"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Ride A Bike"
+        itemArray.append(newItem3)
+        
+       // if let items = defaults.array(forKey: "ToDoListArray") as? [String] {itemArray = items}
         
     }
 
-    var itemArray = ["Find Mike", "Ride a Bike", "Fly a kite"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
@@ -34,7 +46,11 @@ class ViewController: UITableViewController {
         
         let cell  = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -42,14 +58,9 @@ class ViewController: UITableViewController {
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
@@ -64,7 +75,10 @@ class ViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+          
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
---
+++ Todey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todey
+//
+//  Created by River Teles on 11/27/19.
+//  Copyright © 2019 River Teles. All rights reserved.
+//
+
+import Foundation
+class Item{
+    var title: String = ""
+    var done: Bool = false
+    
+}
